b6dc621e7eb09d87e2ddd3d56b83576321979400
Julian Reschke <julian.reschke@gmx.de>
2022-09-07 07:25:52 +0100

    factor out OkHttpClient tests

--- src/test/java/org/greenbytes/http/test1xx/TestHttp1xx.java
+++ src/test/java/org/greenbytes/http/test1xx/TestHttp1xx.java
@@ -126,26 +126,6 @@ public class TestHttp1xx {
         }
     }
 
-    private void testOkHttpClient(Thread server) throws IOException, InterruptedException {
-        try {
-            OkHttpClient client = new OkHttpClient.Builder().protocols(List.of(Protocol.HTTP_1_1)).build();
-            Request request = new Request.Builder().url(TESTURI).get().build();
-            try (Response response = client.newCall(request).execute()) {
-
-                int status = response.code();
-                String body = response.body().string();
-
-                System.err.println("C: status: " + status);
-                System.err.println("C: body: " + escapeLineEnds(body));
-
-                Assert.assertEquals(CONTENT, body);
-                Assert.assertEquals(200, status);
-            }
-        } finally {
-            server.join();
-        }
-    }
-
     protected Thread create100Server() throws IOException {
         return createServer(100, "Continue", null);
     }
@@ -214,31 +194,6 @@ public class TestHttp1xx {
     @Test
     public void testJDKHttpClient200() throws IOException, InterruptedException {
         testJDKHttpClient(create200Server());
-    }
-
-    @Test
-    public void testOkHttpClient100() throws IOException, InterruptedException {
-        testOkHttpClient(create100Server());
-    }
-
-    @Test
-    public void testOkHttpClient102() throws IOException, InterruptedException {
-        testOkHttpClient(create102Server());
-    }
-
-    @Test
-    public void testOkHttpClient103() throws IOException, InterruptedException {
-        testOkHttpClient(create103Server());
-    }
-
-    @Test
-    public void testOkHttpClient199() throws IOException, InterruptedException {
-        testOkHttpClient(create199Server());
-    }
-
-    @Test
-    public void testOkHttpClient200() throws IOException, InterruptedException {
-        testOkHttpClient(create200Server());
     }
 
     public static String readFully(InputStream is) throws IOException {
